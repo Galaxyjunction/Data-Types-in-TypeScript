fb19104ab99fc86fce200343c63096e56f33198b
Galaxyjunction <161058155+Galaxyjunction@users.noreply.github.com>
2024-02-28 22:23:15 -0800

    Add files via upload

---
+++ task1.ts
@@ -0,0 +1,10 @@
+// Give Example of String Data Type in Typescript.
+
+let my_intro: string= "Hello World, my name is Kehkashan";
+console.log(my_intro);
+
+//Give Example of String Data type with CONCATENATION.
+
+let uName: string = "Kehkashan";
+let uLocation: string = "Karachi, Pakistan";
+console.log("Hello World, I am " + uName, "& I live in " + uLocation);
