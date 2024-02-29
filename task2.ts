28112357643096a4e56adfc1677df749901a7b90
Galaxyjunction <161058155+Galaxyjunction@users.noreply.github.com>
2024-02-28 22:24:50 -0800

    Add files via upload

---
+++ task2.ts
@@ -0,0 +1,9 @@
+//Give an Example of Number Data Type and perform arithmetic operation:
+
+let a = 100;
+let b = 50
+console.log('Addition:' , a+b);
+console.log('Subtraction:' , a-b);
+console.log('Multiplication:' , a*b);
+console.log('Division:' , a/b);
+
